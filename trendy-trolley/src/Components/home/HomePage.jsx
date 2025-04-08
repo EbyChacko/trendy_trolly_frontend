fe4102017fdf241c58e3da178af2ef4fd13ff0d5
Eby <cybe1990@gmail.com>
2025-04-08 01:39:52 +0100

    add place holder for the product card

--- trendy-trolley/src/Components/home/HomePage.jsx
+++ trendy-trolley/src/Components/home/HomePage.jsx
@@ -2,25 +2,38 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import CardContainer from './CardContainer';
 import api from '../../api';
-
+import PlaceHolderContainer from '../ui/PlaceHolderContainer';
+import Error from '../ui/Error';
 
 const HomePage = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    
     useEffect(() => {
-        api.get('products')
+        setLoading(true);
+        api.get('products/')
             .then(response => {
-                console.log(response.data);
                 setProducts(response.data);
+                setLoading(false);
+                setError("");
             })
             .catch(error => {
                 console.error('Error fetching products:', error);
+                setLoading(false);
+                setError(error.response?.data?.message || error.message || "An unknown error occurred");
             });
     }, []);
 
     return (
         <>
             <Header />
-            <CardContainer products={ products } />
+            {loading ? (
+                <PlaceHolderContainer />
+            ) : (
+                !error && <CardContainer products={products} />
+            )}
+            {error && <Error error={error} />}
         </>
     );
 };
